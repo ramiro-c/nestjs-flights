bc36b91bb7dab34c894f2d73576244a84648df1e
ramiro <rami992009@gmail.com>
2022-03-08 14:38:15 -0300

    Modulo Passenger: CRUD terminado y logs

--- src/passenger/passenger.controller.ts
+++ src/passenger/passenger.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { IDeleteResponse } from '../interfaces/delete-response.interface';
+import { IPassenger } from '../interfaces/passenger.interface';
+import { PassengerDTO } from './dto/passenger.dto';
+import { PassengerService } from './passenger.service';
 
-@Controller('passenger')
-export class PassengerController {}
+@Controller('api/v1/passenger')
+export class PassengerController {
+  constructor(private readonly passengerService: PassengerService) {}
+
+  @Post()
+  async create(@Body() userDTO: PassengerDTO): Promise<IPassenger> {
+    return await this.passengerService.create(userDTO);
+  }
+
+  @Get()
+  async findAll(): Promise<IPassenger[]> {
+    return await this.passengerService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<IPassenger> {
+    return await this.passengerService.findById(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() userDTO: PassengerDTO,
+  ): Promise<IPassenger> {
+    return await this.passengerService.update(id, userDTO);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<IDeleteResponse> {
+    return await this.passengerService.delete(id);
+  }
+}
--- src/passenger/passenger.service.ts
+++ src/passenger/passenger.service.ts
@@ -1,4 +1,108 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { IPassenger } from '../interfaces/passenger.interface';
+import { PassengerDTO } from './dto/passenger.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PASSENGER } from '../common/models/models';
+import { IDeleteResponse } from '../interfaces/delete-response.interface';
 
 @Injectable()
-export class PassengerService {}
+export class PassengerService {
+  private readonly logger: Logger = new Logger(PassengerService.name, {
+    timestamp: true,
+  });
+
+  constructor(
+    @InjectModel(PASSENGER.name) private readonly model: Model<IPassenger>,
+  ) {}
+
+  async create(passengerDTO: PassengerDTO): Promise<IPassenger> {
+    try {
+      const newPassenger = new this.model(passengerDTO);
+
+      return await newPassenger.save();
+    } catch (e) {
+      if (e.code === 11000) {
+        const message = e.message
+          .substring(
+            e.message.lastIndexOf('{') + 2,
+            e.message.lastIndexOf('}') - 1,
+          )
+          .replaceAll(`"`, `'`);
+
+        throw new ConflictException(
+          `Passenger not created. Duplicated ${message}`,
+        );
+      } else {
+        this.logger.error(
+          `Error creating passenger: ${passengerDTO.email}`,
+          e.stack,
+        );
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async findAll(): Promise<IPassenger[]> {
+    return await this.model.find();
+  }
+
+  async findById(id: string): Promise<IPassenger> {
+    const passenger = await this.model.findById(id);
+
+    if (!passenger) {
+      throw new NotFoundException(`Passenger with ID: ${id} was not found`);
+    }
+
+    return passenger;
+  }
+
+  async update(id: string, passengerDTO: PassengerDTO): Promise<IPassenger> {
+    try {
+      await this.model.findByIdAndUpdate(id, passengerDTO);
+
+      return await this.findById(id);
+    } catch (e) {
+      if (e.code === 11000) {
+        const message = e.message
+          .substring(
+            e.message.lastIndexOf('{') + 2,
+            e.message.lastIndexOf('}') - 1,
+          )
+          .replaceAll(`"`, `'`);
+
+        throw new ConflictException(
+          `Passenger not updated. Already exists ${message}`,
+        );
+      } else {
+        this.logger.error(
+          `Error updating passenger: ${
+            passengerDTO.email
+          } Data: ${JSON.stringify(passengerDTO)}`,
+          e.stack,
+        );
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async delete(id: string): Promise<IDeleteResponse> {
+    const found = await this.model.findByIdAndRemove(id);
+
+    if (!found) {
+      throw new NotFoundException(`Passenger with ID: ${id} was not found`);
+    }
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Passenger deleted successfully',
+    };
+  }
+}
